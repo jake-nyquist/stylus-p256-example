8c2934bc37f641517ac0df68b9a03be424334e3a
Jake Nyquist <jakenyquist@gmail.com>
2023-09-01 18:17:47 +0000

    add basics

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # stylus-c-examples
 examples of c code compiled to stylus
+
+## Building.
+this library requires the stdlib, so we need to use wasi-sdk.
+
+to get it, run the following
+```bash
+export WASI_VERSION=20
+export WASI_VERSION_FULL=${WASI_VERSION}.0
+wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${WASI_VERSION}/wasi-sdk-${WASI_VERSION_FULL}-linux.tar.gz
+tar xvf wasi-sdk-${WASI_VERSION_FULL}-linux.tar.gz
+
+/// then, once installed, build with this:
+
+export WASI_SDK_PATH=`pwd`/wasi-sdk-${WASI_VERSION_FULL}
+CC="${WASI_SDK_PATH}/bin/clang --sysroot=${WASI_SDK_PATH}/share/wasi-sysroot"
+$CC *.c -o out/p256.wasm -mbulk-memory -Wl,--no-entry -mexec-model=reactor -O3
+```
+The wasisdk compiler seems to ignore the --no-entry flag, unless we use the `-mexec-model=reactor`
+
+## deploying
+
+install the dependency for cargo-stylus, which is the deployer
+``` cargo install cargo-stylus```
+
+```cargo stylus deploy --wasm-file-path=out/p256.wasm``````
+
+this version is deployed at: 0xff92a41a7edb4af7862868031121dbdba3319270385ed3b8b49544c118757fa7
---
+++ main.c
@@ -0,0 +1,47 @@
+#include "./p256/p256.c"
+#include "./stylus-lib/stylus.h"
+
+ArbResult user_main(uint8_t * args, size_t args_len) {
+    const uint8_t * sig = args;
+    const uint8_t * pk = args + 16;
+    const uint8_t * input = args + 32;
+    const uint64_t length = args_len - 48;
+
+    // call p256_verify on the results
+    p256_ret_t res = p256_verify(input, length, sig, pk);
+
+    if (res == P256_INVALID_SIGNATURE) {
+
+        // simply return a failure here 
+        return (ArbResult) {
+            .status = Failure,
+            .output = args,
+            .output_len = 0,
+        };
+    }
+
+    return (ArbResult) {
+        .status = Success,
+        .output = args,
+        .output_len = 0,
+    };
+}
+
+
+// sets user_main as the entrypoint
+ENTRYPOINT(user_main);
+
+
+// define these two curves as a hack -- doesn't like the reactor exec model without them
+// DO NOT USE
+const br_ec_curve_def br_secp521r1 = {
+	BR_EC_secp256r1,
+	P256_N, sizeof P256_N,
+	P256_G, sizeof P256_G
+};
+
+const br_ec_curve_def br_secp384r1 = {
+	BR_EC_secp256r1,
+	P256_N, sizeof P256_N,
+	P256_G, sizeof P256_G
+};
